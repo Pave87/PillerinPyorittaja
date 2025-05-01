70b192a068d372261c4e892841af4f61b0dba8d9
Pave87 <aki1987@kapsi.fi>
2025-05-01 23:58:17 +0300

    Gui fixes New version popup

---
+++ MauiBlazorHybrid/Services/ISettingsService .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Maui.Storage;
+
+namespace MauiBlazorHybrid.Services
+{
+    public interface ISettingsService
+    {
+        bool IsDebugEnabled { get; set; }
+        event EventHandler SettingsChanged;
+    }
+
+    public class SettingsService : ISettingsService
+    {
+        private const string DEBUG_KEY = "debug_enabled";
+        public event EventHandler? SettingsChanged;
+
+        public bool IsDebugEnabled
+        {
+            get => Preferences.Default.Get(DEBUG_KEY, false);
+            set
+            {
+                Preferences.Default.Set(DEBUG_KEY, value);
+                SettingsChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
